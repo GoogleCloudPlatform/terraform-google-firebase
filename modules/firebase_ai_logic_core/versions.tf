3abf70f5a5ddd2fd97de2f2aa041ccf1d9605fb8
Tri Cao <caot@google.com>
2026-03-29 16:48:30 +0700

    chore: release v0.2.1

--- modules/firebase_ai_logic_core/versions.tf
+++ modules/firebase_ai_logic_core/versions.tf
@@ -32,10 +32,10 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-firebase:firebase_ai_logic_core/v0.2.0"
+    module_name = "blueprints/terraform/terraform-google-firebase:firebase_ai_logic_core/v0.2.1"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-firebase:firebase_ai_logic_core/v0.2.0"
+    module_name = "blueprints/terraform/terraform-google-firebase:firebase_ai_logic_core/v0.2.1"
   }
 }
 
