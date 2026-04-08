d0c4033c6bd3e4abcd41c4b7516ed8020c59fa18
Tri Cao <caot@google.com>
2026-04-09 03:44:34 +0700

    feat: Enable Firebase Hosting API and release v0.2.3 (#18)

--- modules/firebase_auth/versions.tf
+++ modules/firebase_auth/versions.tf
@@ -25,10 +25,10 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-firebase:firebase_auth/v0.2.2"
+    module_name = "blueprints/terraform/terraform-google-firebase:firebase_auth/v0.2.3"
   }
   provider_meta "google-beta" {
-    module_name = "blueprints/terraform/terraform-google-firebase:firebase_auth/v0.2.2"
+    module_name = "blueprints/terraform/terraform-google-firebase:firebase_auth/v0.2.3"
   }
 }
 
